78d9a45d1d8845d319dec5c5da96b6302328fe53
Aran Borkum <aranborkum@hotmail.com>
2022-12-29 13:15:09 +0000

    Adding login and registration pages

--- application/views.py
+++ application/views.py
@@ -7,15 +7,20 @@ view = Blueprint("views", __name__)
 def index():
     return render_template("index.html")
 
-# @view.route("/login", methods=["POST", "GET"])
-# def login():
-#     # if request.method == "POST":
-#     #
-#     #     user, message = login_procedure(request.form, DB_URI)
-#     #
-#     #     if user is not None:
-#     #         login_user(user, remember=True, duration=datetime.timedelta(hours=1))
-#     #         flash("Logged in successfully.")
-#     #         return redirect(url_for("chat"))
-#
-#     return render_template("login.html")
+
+@view.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+    # if request.method == "POST":
+    #
+    #     user, message = login_procedure(request.form, DB_URI)
+    #
+    #     if user is not None:
+    #         login_user(user, remember=True, duration=datetime.timedelta(hours=1))
+    #         flash("Logged in successfully.")
+    #         return redirect(url_for("chat"))
+
+
+@view.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
