af505eda03ee60a3d418ab1b54f6813399cf512d
Aran Borkum <aranborkum@hotmail.com>
2022-12-29 12:31:50 +0000

    Initial commit, everything working

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config["SECRET"] = "secret_key"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on("message")
+def handle_message(message):
+    print(f"Received message: {message}")
+    if message != "User connected!":
+        send(message, broadcast=True)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost", debug=True, allow_unsafe_werkzeug=True)
